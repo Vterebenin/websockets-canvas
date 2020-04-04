5794ed6e9a513fb3ea27f0e818d5e373c935117d
Vterebenin <terebenin.work@gmail.com>
2020-04-04 20:17:01 +0300

    feat: мигрировал передвижения на phaser 3

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,10 +3,10 @@
 </template>
 
 <script>
-// import io from 'socket.io-client'
+import io from 'socket.io-client'
 import ph from 'phaser'
 // import { initKeys } from '@/sockets/helpers'
-import { Updater, Player, Map } from '@/sockets/engine/classes'
+import { Updater, Map } from '@/sockets/engine/classes'
 
 export default {
   data () {
@@ -19,7 +19,6 @@ export default {
     }
   },
   created () {
-    // this.socket = io('http://localhost:8000')
     // this.updater.socket = this.socket
   },
   mounted () {
@@ -44,16 +43,14 @@ export default {
       }
     }
     const game = new ph.Game(config)
+    console.log(game)
+    const self = this
 
     function preload () {
       this.load.image('background', '/assets/swamplands/2 Background/Background.png')
       this.load.tilemapTiledJSON('map', '/tileMaps/json/testmap.json')
       this.load.image('Tileset', '/assets/swamplands/1 Tiles/Tileset.png')
       this.load.image('player', '/assets/characters/2 GraveRobber/GraveRobber.png')
-      // this.load.image('ground', 'src/games/firstgame/assets/platform.png')
-      // this.load.image('star', 'src/games/firstgame/assets/star.png')
-      // this.load.image('bomb', 'src/games/firstgame/assets/bomb.png')
-      // this.load.spritesheet('dude', 'src/games/firstgame/assets/dude.png', { frameWidth: 32, frameHeight: 48 })
     }
 
     function create () {
@@ -62,86 +59,91 @@ export default {
       bg.displayHeight = gameField.height
 
       this.map = this.add.tilemap('map')
-
       const TileSet = this.map.addTilesetImage('Tileset', 'Tileset')
-
       this.backgroundLayer = this.map.createStaticLayer('Слой тайлов 1', TileSet)
-      this.player = this.physics.add.sprite(gameField.width / 2, gameField.height / 2, 'player')
       this.backgroundLayer.setCollisionBetween(1, 50)
-      this.physics.add.collider(this.player, this.backgroundLayer)
+
+      this.otherPlayers = this.physics.add.group()
+
+      self.socket = io('http://localhost:8000')
+      self.socket.on('currentPlayers', (players) => {
+        for (const [id, player] of Object.entries(players)) {
+          if (id === self.socket.id) {
+            self.addPlayer(this, player)
+          } else {
+            self.addOtherPlayer(this, player)
+          }
+        }
+      })
+      self.socket.on('newPlayer', (playerInfo) => {
+        self.addOtherPlayer(this, playerInfo)
+      })
+      self.socket.on('disconnect', (playerId) => {
+        this.otherPlayers.getChildren().forEach((otherPlayer) => {
+          if (playerId === otherPlayer.playerId) {
+            otherPlayer.destroy()
+          }
+        })
+      })
+
       this.wasd = {
         up: this.input.keyboard.addKey(ph.Input.Keyboard.KeyCodes.W),
         down: this.input.keyboard.addKey(ph.Input.Keyboard.KeyCodes.S),
         left: this.input.keyboard.addKey(ph.Input.Keyboard.KeyCodes.A),
         right: this.input.keyboard.addKey(ph.Input.Keyboard.KeyCodes.D)
       }
-      console.log(this.wasd)
+      self.socket.on('playerMoved', (playerInfo) => {
+        this.otherPlayers.getChildren().find((otherPlayer) => {
+          if (playerInfo.playerId === otherPlayer.playerId) {
+            otherPlayer.setPosition(playerInfo.x, playerInfo.y)
+            return true
+          }
+        })
+      })
     }
 
     function update () {
-      if (this.wasd.left.isDown) {
-        this.player.setVelocityX(-160)
-      } else if (this.wasd.right.isDown) {
-        this.player.setVelocityX(160)
-      } else {
-        this.player.setVelocityX(0)
-      }
-      if (this.wasd.up.isDown && this.player.body.blocked.down) {
-        this.player.setVelocityY(-330)
-      }
-    }
-
-    console.log(game)
-
-    // const canvas = this.$refs.game
-    // if (canvas.getContext) {
-    //   this.updater.ctx = canvas.getContext('2d')
-    //
-    //   initKeys(this.keys)
-    //   this.updater.keys = this.keys
-    //
-    //   this.socket.on('setMap', (map) => {
-    //     this.handleSetMap(canvas, map)
-    //   })
-    //   this.socket.on('currentPlayers', (players, id) => {
-    //     this.handleCurrentPlayers(players, id)
-    //   })
-    //   this.socket.on('newPlayer', (players, id) => {
-    //     this.handleNewPlayers(players, id)
-    //   })
-    //   this.socket.on('disconnect', (id) => {
-    //     this.handleDisconnect(id)
-    //   })
-    //   this.updater.init()
-    // }
-  },
-  methods: {
-    handleCurrentPlayers (players, id) {
-      for (const [_id, _player] of Object.entries(players)) {
-        if (_id === id) {
-          this.clientPlayers.push(new Player({ ..._player, isEnemy: false }))
+      if (this.player) {
+        if (this.wasd.left.isDown) {
+          this.player.setVelocityX(-160)
+        } else if (this.wasd.right.isDown) {
+          this.player.setVelocityX(160)
         } else {
-          this.clientPlayers.push(new Player({ ..._player, isEnemy: true }))
+          this.player.setVelocityX(0)
+        }
+        if (this.wasd.up.isDown && this.player.body.blocked.down) {
+          this.player.setVelocityY(-330)
+        }
+        const x = this.player.x
+        const y = this.player.y
+        if (
+          this.player.oldPosition &&
+          (
+            x !== this.player.oldPosition.x ||
+            y !== this.player.oldPosition.y
+          )
+        ) {
+          self.socket.emit('playerMovement', { x, y })
+        }
+
+        // save old position data
+        this.player.oldPosition = {
+          x: this.player.x,
+          y: this.player.y
         }
       }
-      this.updater.players = this.clientPlayers
+    }
+  },
+  methods: {
+    addPlayer (game, playerInfo) {
+      game.player = game.physics.add.sprite(playerInfo.x, playerInfo.y, 'player')
+      game.physics.add.collider(game.player, game.backgroundLayer)
     },
-    handleNewPlayers (players, id) {
-      this.clientPlayers.push(new Player({ ...players[id], isEnemy: true }))
-      this.updater.players = this.clientPlayers
-    },
-    handleDisconnect (id) {
-      this.clientPlayers.filter(el => el.id !== id)
-      this.updater.players = this.clientPlayers
-    },
-    handleSetMap (canvas, map) {
-      canvas.width = map.gameField.width
-      canvas.height = map.gameField.height
-      this.updater.map = map
-    },
-    create () {
-    },
-    update () {
+    addOtherPlayer (game, playerInfo) {
+      const otherPlayer = game.add.sprite(playerInfo.x, playerInfo.y, 'player')
+      otherPlayer.playerId = playerInfo.playerId
+      game.otherPlayers.add(otherPlayer)
+      game.physics.add.collider(otherPlayer, game.backgroundLayer)
     }
   }
 }
--- frontend/sockets/app.js
+++ frontend/sockets/app.js
@@ -1,4 +1,3 @@
-const { Player, Map, Block, SpellBook, Spell } = require('./engine/classes')
 const Express = require('express')()
 const Http = require('http').Server(Express)
 const Socketio = require('socket.io')(Http)
@@ -8,49 +7,49 @@ let players = {}
 Http.listen(8000, () => {
     console.log('Listening at :8000...')
 })
-const map = new Map({ name: 'Blind forest' })
-map.blocks.push(new Block({
-  width: 300,
-  height: 50,
-  position: { x: 400, y: map.gameField.height - 50 },
-  color: 'pink'
-}))
-map.blocks.push(new Block({ width: 200, position: { x: 150, y: map.gameField.height - 100 }, color: 'red' }))
-map.blocks.push(new Block({ width: 500, position: { x: 200, y: 300 }, color: 'green' }))
-map.blocks.push(new Block({ width: 500, position: { x: 200, y: 300 }, color: 'black' }))
 
 Socketio.on('connection', function (socket) {
-  const { id } = socket
-  players[id] = new Player({ id })
-  const attackSpell = new Spell({
-    mechanic (player) {
-      console.log('casted!', player.position, id)
-    }
-  })
-  players[id].spellBook = new SpellBook({
-    attackSpell,
-    player: { ...players[id] }
-  })
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+  }
+  // const attackSpell = new Spell({
+  //   mechanic (player) {
+  //     console.log('casted!', player.position, id)
+  //   }
+  // })
+  // players[id].spellBook = new SpellBook({
+  //   attackSpell,
+  //   player: { ...players[id] }
+  // })
 
 
-  socket.emit('setMap', map)
-  socket.emit('currentPlayers', players, id, map)
-  socket.broadcast.emit('newPlayer', players, id)
+  // socket.emit('setMap', map)
+  socket.emit('currentPlayers', players)
+  socket.broadcast.emit('newPlayer', players[socket.id])
 
-  socket.on('keyPressed', (keys, id) => {
-    players[id] = players[id].handleKeys(keys)
+  socket.on('playerMovement', ({ x, y }) => {
+    players[socket.id].x = x
+    players[socket.id].y = y
+    socket.broadcast.emit('playerMoved', players[socket.id])
   })
-  setInterval(() => {
-    if (players[id]) {
-      players[id].handleMovements()
-      for (const block of map.blocks) {
-        if (block.solid) {
-          players[id].colCheck(block)
-        }
-      }
-    }
-    socket.emit('update', players)
-  }, 1000 / 60)
+  // socket.on(,'keyPressed', (keys, id) => {
+  //   players[id] = players[id].handleKeys(keys)
+  // })
+  // setInterval(() => {
+  //   if (players[id]) {
+  //     players[id].handleMovements()
+  //     for (const block of map.blocks) {
+  //       if (block.solid) {
+  //         players[id].colCheck(block)
+  //       }
+  //     }
+  //   }
+  //   socket.emit('update', players)
+  // }, 1000 / 60)
   socket.on('disconnect', function () {
     delete players[socket.id]
     Socketio.emit('disconnect', socket.id)
